260cc9c98c8fe9fd176ae0f8e07cab22d3db35e1
toringo <toringona@163.com>
2021-08-17 11:33:27 +0000

    Deploying to gh_pages from @ okfe/chainlist@549b92c547949e0dd68f11e14133dd040baf0d77 🚀

---
+++ _next/static/0aBhXSDTzzUQl1cDDVLgf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Ff4f622062b89a868673a.css","static\u002Fchunks\u002Fpages\u002Findex-43cb580126ac99ff5951.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1fc2e2e19174e25bcb52.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
